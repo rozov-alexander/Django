e896a0c5d07d00caa8e34ce17770f90670fe0d13
Rozov Alexander <a_rozov@internet.ru>
2021-12-12 21:03:16 +0300

    Небольшое исправление с добавлением products в цикле

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 
 menu_links = [
-        {'view_name': 'index', 'name': 'Домой'},
-        {'view_name': 'products', 'name': 'Продукты'},
-        {'view_name': 'contact', 'name': 'Контакты'},
-    ]
+    {'view_name': 'index', 'name': 'Домой'},
+    {'view_name': 'products', 'name': 'Продукты'},
+    {'view_name': 'contact', 'name': 'Контакты'},
+]
+
 
 def index(request):
     return render(request, 'mainapp/index.html', context={'menu_links': menu_links, 'title': 'главная'})
@@ -16,4 +17,9 @@ def contact(request):
 
 
 def products(request):
-    return render(request, 'mainapp/products.html', context={'menu_links': menu_links, 'title': 'продукты'})
+    products = [
+        {'product_name': 'product1', 'img': 'img/product-11.jpg', 'desc': 'Стул повышенного качества'},
+        {'product_name': 'product2', 'img': 'img/product-21.jpg', 'desc': 'Стул чёткий'},
+        {'product_name': 'product3', 'img': 'img/product-31.jpg', 'desc': 'Стул третий'},
+    ]
+    return render(request, 'mainapp/products.html', context={'menu_links': menu_links, 'products': products, 'title': 'продукты'})
