016a6065974b438a82874e8b87e089fe711c9036
Rozov Alexander <a_rozov@internet.ru>
2021-12-12 17:54:58 +0300

    Четверть 02. Урок 02. Домашнее задание выполнено

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 
+
+menu_links = [
+        {'view_name': 'index', 'name': 'Домой'},
+        {'view_name': 'products', 'name': 'Продукты'},
+        {'view_name': 'contact', 'name': 'Контакты'},
+    ]
+
 def index(request):
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', context={'menu_links': menu_links, 'title': 'главная'})
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    return render(request, 'mainapp/contact.html', context={'menu_links': menu_links, 'title': 'контакты'})
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    return render(request, 'mainapp/products.html', context={'menu_links': menu_links, 'title': 'продукты'})
