093780badafb3682e7fcf92bb0395ea8f4a616b2
Rozov Alexander <a_rozov@internet.ru>
2022-01-08 23:32:26 +0300

    Исправил скрипт, на тот что был на уроке

--- geekshop/static/js/cart_scripts.js
+++ geekshop/static/js/cart_scripts.js
@@ -1,15 +1,12 @@
 window.onload = function () {
-    $('.cart_list').on('click', 'input[type="number"]', function (event) {
-        var t_href = event.target;
+    document.querySelector('.basket_list input[type="number"]').addEventListener('click', function (event) {
+        const href = event.target;
 
-        $.ajax({
-            url: "/cart/edit/" + t_href.name + "/" + t_href.value + "/",
-
-            success: function (data) {
-                $('.cart_list').html(data.result);
-            },
-        });
-
-        event.preventDefault ();
-    });
+        fetch(`/cart/edit/${href.name}/${href.value}/`)
+            .then((data) => data.json())
+            .then((json) => {
+                document.querySelectorAll("basket_list").innerHTML = json.result;
+            })
+        event.preventDefault();
+    })
 }
